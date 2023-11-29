1b220d8184703c9a9fee5ae23b2374639d158c3d
felippecardoso7 <felippecardoso54@gmail.com>
2023-11-29 10:57:50 -0300

    funcionarios commit

---
+++ trabalho.py
@@ -0,0 +1,20 @@
+# Funcionarios Da impresa
+
+# Estrutura dic
+funcionario ={'Nome': '', 'Salario':0.0, 'Ativo':False}
+
+# Entrada
+print('\n\t\t -- Funcionarios do Registro --\n')
+funcionario['Nome'] =input('informe o nome: ')
+funcionario['Salario']=float(input(('informe o salario: ')))
+funcionario['Ativo'] = True
+
+
+# Saida
+print(f'Nome.......{funcionario["Nome"]}')
+print(f'Salario.......{funcionario["Salario"]}')
+print(F' Funcionarios Ativo........{funcionario["Ativo"]}')
+
+
+
+
